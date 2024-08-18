0d1bbfd5799c74d333b9507e29ce55104686880e
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-18 18:24:27 +0330

    find your favorie drink right here right now

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@ document.querySelector('input').addEventListener('click',getFetch)
 //let datito = Math.floor(Math.random() * 1000)
 
 function getFetch(){
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY`)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        let choice = Math.floor(Math.random() * data.photos.length)
-        document.querySelector('h2').innerText = data.photos[choice].rover.name
-        document.querySelector('img').src = data.photos[choice].img_src
-        document.querySelector('#date').innerText = data.photos[choice].earth_date
-        
+        console.log(data.drinks)
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('#drinkCategory').innerText = data.drinks[0].strCategory
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('#alcoholic').innerText = data.drinks[0].strAlcoholic
+        document.querySelector('#glass').innerText = data.drinks[0].strGlass
+        document.querySelector('#instr').innerText = data.drinks[0].strInstructions
     })
 }
